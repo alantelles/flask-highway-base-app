1c03f3118fa36e1959a75cb110fbf7ccad4b6d70
alantelles <alantelles86@gmail.com>
2020-12-11 09:37:06 -0300

    start implementing login

--- app/blueprints/user_management/views/access_control.py
+++ app/blueprints/user_management/views/access_control.py
@@ -1,5 +1,6 @@
-from flask import request, session, redirect, url_for
+from flask import request, session, redirect, url_for, flash
 from app import app
+from app.blueprints.user_management.models.user import User
 
 admin_role_name = app.config.get('ADMIN_ROLE_NAME', 'admin')
 
@@ -20,9 +21,44 @@ def roles_allowed(role_list):
 def only_admin(fn):
     def wrapper(self, *args, **kwargs):
         print("only admin can view this")
-        if 'admin' == admin_role_name:
-            return fn(self, *args, **kwargs)
+        logged = session.get('logged')
+        if logged:
+            if logged.get('is_admin'):
+                return fn(self, *args, **kwargs)
+            else:
+                flash('This page is only allowed to system administrator', 'info')
+                return redirect(url_for('index'))
         else:
             return redirect(url_for('login'))
 
     return wrapper
+
+def try_login_user(form_data):
+    user = User.query.filter_by(username=form_data['username']).first()
+
+    if user.check_password(form_data['password']):
+        session['logged'] = {}
+        session['logged']['id'] = user.id
+        session['logged']['roles'] = [role.id for role in user.roles]
+        role_names = [role.name for role in user.roles]
+        session['logged']['is_admin'] = admin_role_name in role_names
+        print(session['logged'])
+        return redirect(url_for('index'))
+
+    else:
+        flash("Invalid credentials. Can't log you in.", "danger")
+        return redirect(url_for('login'))
+
+def logout_user():
+    session['logged'] = None
+    return redirect(url_for('login'))
+
+class AccessController:
+    def is_anonymous():
+        return not session.get('logged', False)
+
+@app.context_processor
+def session_helpers():
+    return dict(
+        access=AccessController
+    )
