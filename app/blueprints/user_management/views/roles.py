014c67bffcbc1ab05cf2f88b74a2d6b313beb236
alantelles <alantelles86@gmail.com>
2020-12-11 02:19:43 -0300

    show flash messages on main view

--- app/blueprints/user_management/views/roles.py
+++ app/blueprints/user_management/views/roles.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, request,url_for
+from flask import render_template, redirect, request, url_for, session, flash
 from app import db
 from app.blueprints.user_management.models.role import Role
 
@@ -24,10 +24,18 @@ class RolesViews:
         return render_template('user_management/roles/show.html', role=role)
 
     def edit(self, id):
+        session['now_edited'] = id
         role = Role.query.get(id)
         return render_template('user_management/roles/edit.html', role=role)
 
     def update(self, id):
-        return 'updating'
+        if session.get('now_edited', None) == id:
+            role = Role.query.get(id)
+            role.name = request.form['name']
+            db.session.commit()
+            flash(f'Role {role.name} edited successfully', 'success')
+            return redirect(url_for('user_management.roles.show', id=id))
+        else:
+            return 'Irregular editing try'
 
 roles_views = RolesViews()
