774b89bee85f6bb4375f96188ce3c73b8535d0c8
alantelles <musicadepreto@gmail.com>
2020-12-21 11:32:44 -0300

    WIP: tokens

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 
-app = Flask(__main__)
+app = Flask(__name__)
 
 from app import config
 
@@ -11,12 +11,9 @@ db = SQLAlchemy(app)
 
 from app import app_views 
 
-@app.context_processor
-def inject_dict_for_all_templates():
-    return dict(go_away='go away string')
+
 
 #DON'T REMOVE: blueprints register section
 from app.blueprints.user_management import user_management
 app.register_blueprint(user_management)
 #END: blueprints register section
-
--- app/blueprints/user_management/__init__.py
+++ app/blueprints/user_management/__init__.py
@@ -4,6 +4,7 @@ from flask import Blueprint, render_template, request
 from app.dont_touch.helpers.routes import register_routes
 
 #DON'T REMOVE: blueprint views register section
+from app.blueprints.user_management.views.tokens import tokens_views
 from app.blueprints.user_management.views.users import users_views
 from app.blueprints.user_management.views.roles import roles_views
 from app.blueprints.user_management.views.audits import audits_views
@@ -25,5 +26,5 @@ class UserManagementViews:
 user_management_views = UserManagementViews()
 
 #DON'T TOUCH: register routes section
-register_routes('user_management', user_management_views=user_management_views, users=users_views, roles=roles_views, audits=audits_views)
+register_routes('user_management', user_management_views=user_management_views, users=users_views, roles=roles_views, audits=audits_views, tokens=tokens_views)
 #END: register routes section
